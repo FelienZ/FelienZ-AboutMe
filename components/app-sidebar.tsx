ef21c354b9721a979bf93743e1b4a7af9dfe700c
FelienZ <rajabfadh@gmail.com>
2025-11-09 11:11:40 +0700

    improve sidebar components

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,8 +1,9 @@
-import { Calendar, Contact, Home, PanelsTopLeft, Profile, Search, Settings, User } from "lucide-react"
+import { ChevronUp, Contact, Home, icons, PanelsTopLeft, Settings, User, User2 } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -10,41 +11,54 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
 
 // Menu items.
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "/",
     icon: Home,
   },
   {
     title: "My Profile",
-    url: "#",
+    url: "/profile",
     icon: User,
   },
   {
     title: "My Projects",
-    url: "#",
+    url: "/projects",
     icon: PanelsTopLeft,
   },
   {
     title: "Contact Me",
-    url: "#",
+    url: "/contact",
     icon: Contact,
   },
 ]
 
+const dropdownItems = [
+    {
+        title: 'My Account',
+        icon: User,
+        url: '/profile'
+    },
+    {
+        title: 'Settings',
+        icon: Settings,
+        url: '/settings'
+    },
+]
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
+        <SidebarGroup className="h-full">
+          <SidebarGroupLabel className="font-bold text-sm">{'Rajabfadh Portofolio'.toUpperCase()}</SidebarGroupLabel>
+          <SidebarGroupContent className="h-full">
+            <SidebarMenu className="mt-5 gap-4 divide-y">
+                {items.map((item) => (
+                <SidebarMenuItem key={item.title} className="pb-2">
                   <SidebarMenuButton asChild>
                     <a href={item.url}>
                       <item.icon />
@@ -57,6 +71,35 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <User2 /> Username
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[18dvw] border p-3 gap-2 flex flex-col"
+                >
+                  {dropdownItems.map((item, idx) => 
+                    <DropdownMenuItem key={idx}>
+                        <SidebarMenuButton asChild>
+                        <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                        </a>
+                    </SidebarMenuButton>
+                    </DropdownMenuItem>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
     </Sidebar>
   )
 }
