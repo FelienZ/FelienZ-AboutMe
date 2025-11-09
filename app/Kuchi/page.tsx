9776f41e7a291eaa8941f86d2c7fc5ce30526e6e
FelienZ <rajabfadh@gmail.com>
2025-11-09 09:31:55 +0700

    add sidebar component

--- app/Kuchi/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Kuchi(){
-    return(
-        <h1>Ini Next Punya Kuchi</h1>
-    )
-}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import { Calendar, Contact, Home, PanelsTopLeft, Profile, Search, Settings, User } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "My Profile",
+    url: "#",
+    icon: User,
+  },
+  {
+    title: "My Projects",
+    url: "#",
+    icon: PanelsTopLeft,
+  },
+  {
+    title: "Contact Me",
+    url: "#",
+    icon: Contact,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
